5b5e3abf9a3e0fa0d8e95d945568915c2ae32d00
Sankalan Dasgupta <sankalandasgupta@gmail.com>
2023-11-10 23:08:45 +0530

    implement concurrent draw and config functionality

--- index.js
+++ index.js
@@ -16,6 +16,16 @@ const io = new Server(httpServer, {
 
 io.on("connection", (socket) => {
   console.log("connected", socket.id);
+
+  socket.on("beginPath", (arg) => {
+    socket.broadcast.emit("beginPath", arg);
+  });
+  socket.on("drawPath", (arg) => {
+    socket.broadcast.emit("drawPath", arg);
+  });
+  socket.on("config", (arg) => {
+    socket.broadcast.emit("config", arg);
+  });
 });
 
 httpServer.listen(8080);
